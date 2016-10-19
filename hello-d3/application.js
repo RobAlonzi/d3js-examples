fc2006620088633c5b1b45f0f73f9257d3f709bb
Rob Alonzi <alonzi.rob@gmail.com>
2016-10-18 23:17:37 -0400

    First Commit

---
+++ hello-d3/application.js
@@ -0,0 +1,9 @@
+let svg = d3.select("body").append("svg")
+  .attr("width", 250)
+  .attr("height", 250);
+
+let rect = svg.append("rect")
+    .attr("x", 50)
+    .attr("y", 50)
+    .attr("width", 20)
+    .attr("height", 20)
